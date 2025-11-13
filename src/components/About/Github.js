53128030c98067872a6f52dfd279fa8fccfb6d5a
Magdalin Princy <princyrajadurai@gmail.com>
2025-11-13 21:29:31 +0530

    commented a text

--- src/components/About/Github.js
+++ src/components/About/Github.js
@@ -15,9 +15,9 @@ function Github() {
         color="#c084f5"
         fontSize={16}
       />
-      <p style={{ color: "gray", fontSize: "14px", marginTop: "10px", textAlign: "center" }}>
+      {/* <p style={{ color: "gray", fontSize: "14px", marginTop: "10px", textAlign: "center" }}>
         Calendar displays only public contributions
-      </p>
+      </p> */}
     </Row>
   );
 }
